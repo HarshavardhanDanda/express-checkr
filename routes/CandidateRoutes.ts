a158566da6821ee3df62d5511eba1b3a951bb990
HarshavardhanDanda <harshavardhan.danda@zemosolabs.com>
2024-03-26 08:27:34 +0530

    added more validations

--- routes/CandidateRoutes.ts
+++ routes/CandidateRoutes.ts
@@ -17,13 +17,34 @@ import {
   getCandidateDetailsByCandidateId,
 } from "../controllers/CandidateDetailsController";
 import { isAuth } from "../middleware/isAuth";
+import { body, param } from "express-validator/check";
 
 const candidateRouter = express.Router();
 
 candidateRouter.get("/", isAuth, getAllCandidates);
 candidateRouter.get("/:candidateId", isAuth, getCandidateById);
-candidateRouter.post("/", isAuth, createCandidate);
-candidateRouter.put("/:candidateId", isAuth, updateCandidate);
+candidateRouter.post(
+  "/",
+  isAuth,
+  [
+    body("name").trim().not().isEmpty(),
+    body("adjudication").isIn(["adverse_action", "engage"]),
+    body("status").isIn(["clear", "consider", "scheduled"]),
+    body("location").trim().not().isEmpty(),
+  ],
+  createCandidate
+);
+candidateRouter.put(
+  "/:candidateId",
+  isAuth,
+  [
+    body("name").trim().not().isEmpty(),
+    body("adjudication").isIn(["adverse_action", "engage"]),
+    body("status").isIn(["clear", "consider", "scheduled"]),
+    body("location").trim().not().isEmpty(),
+  ],
+  updateCandidate
+);
 candidateRouter.delete("/:candidateId", isAuth, deleteCandidate);
 
 // <===========================================================>
@@ -37,6 +58,11 @@ candidateRouter.get(
 candidateRouter.post(
   "/:candidateId/adverse",
   isAuth,
+  [
+    param('candidateId').isUUID(), // Validate that candidateId is a valid UUID
+    body('name').trim().not().isEmpty(),
+    body('status').equals('scheduled'),
+  ],
   createAdverseActionForCandidate
 );
 candidateRouter.delete(
@@ -58,6 +84,22 @@ candidateRouter.get(
   isAuth,
   getCandidateDetailsByCandidateId
 );
-candidateRouter.post("/:candidateId/details", isAuth, createCandidateDetails);
+candidateRouter.post(
+  "/:candidateId/details",
+  isAuth,
+  [
+    param("candidateId").isUUID(), // Validate that candidateId is a valid UUID
+    body("email").isEmail().normalizeEmail(),
+    body("date_of_birth").optional().isISO8601(), // Assuming date_of_birth is optional and should be in ISO8601 format
+    body("phone_no").optional().isNumeric(),
+    body("drivers_licence").optional().trim(),
+    body("social_security").optional().trim(),
+    body("package").optional().trim(),
+    body("report_created_time").optional().isISO8601(), // Assuming report_created_time is optional and should be in ISO8601 format
+    body("report_completed_time").optional().isISO8601(), // Assuming report_completed_time is optional and should be in ISO8601 format
+    body("turn_around_time").optional().isISO8601(), // Assuming turn_around_time is optional and should be in ISO8601 format
+  ],
+  createCandidateDetails
+);
 
 export default candidateRouter;
